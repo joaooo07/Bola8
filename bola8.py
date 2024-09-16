a20fe673fc18045349ddd98de52878a5270d783d
João Motta <138726871+joaooo07@users.noreply.github.com>
2024-09-16 14:33:34 -0300

    Add files via upload

---
+++ bola8.py
@@ -0,0 +1,6 @@
+import random
+while True:
+    lista = ['sim', 'sim', 'certeza!','certeza!','nao','NUNCA']
+    escolha = random.choice(lista)
+    n1 =str(input("faça sua pergunta: "))
+    print(escolha)
